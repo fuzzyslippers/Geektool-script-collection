f1fce50053eef8471c3dd20ff2bc6680e98bfd9e
fuzzyslippers <yoshi445511@gmail.com>
2010-05-04 17:11:29 -0400

    Added conditions which gets the weather conditions. md5-sums for current versions will be kept in md5-sums.

---
+++ conditions.sh
@@ -0,0 +1,20 @@
+function help() {
+	echo "conditions.sh [-z ZIPCODE]"
+	echo "USAGE:"
+	echo "	z: Puts the zipcode entered in the URL."
+	echo "ABOUT:"
+	echo "	This script gets the weather conditions for a specified zipcode."
+	echo "	Author: Nick Ippoliti"
+	echo "	MD5 hash is in conditions.md5"
+	echo "	Oh, and the zipcode of Beverly Hills is 90210."
+}
+clear;
+while getopts "z:h" arg; do
+	case "$arg" in
+		z) ZIPCODE="$OPTARG";;
+		h) eval help;;
+	esac
+done
+curl --silent "http://www.wunderground.com/cgi-bin/findweather/getForecast?query="$ZIPCODE -o temp1
+grep '<div class="b" style="font-size: 14px;">.*</div>' temp1 | sed 's@<div class="b" style="font-size: 14px;">@@'|sed 's@</div>@@' |sed 's@		@@'
+rm -rf temp1
